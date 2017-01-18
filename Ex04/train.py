be45f6a7bde6e463b0784d5babc6570b1b60b46e
Muhammad Hisham Hamed <mhmd.h.hamed@gmail.com>
2017-01-18 20:30:13 +0100

    Update train.py
    remove transpose

--- Ex04/train.py
+++ Ex04/train.py
@@ -5,7 +5,7 @@ import json
 import random
 from keras.models import model_from_json
 from keras.models import Sequential
-from keras.layers.core import Dense, Flatten
+from keras.layers.core import Dense, Flatten,Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.initializations import normal
 from keras.optimizers import Adam,RMSprop
@@ -24,18 +24,18 @@ explore = 10000 # annealing of the epsilon (random actions are more at first)
 epsilon = INITIAL_EPSILON
 test_steps = 3000 # test evey 3000 steps
 
-# do some trajectories with only greedy actions and see the sum of the rewards is decreasing or not
+#CNN model 
 def model():
     input_shape = (img_rows,img_cols,opt.hist_len)
 
     model = Sequential()
-    model.add(Convolution2D(32,4,4,subsample=(2,2),activation='relu',
+    model.add(Convolution2D(32,4,4,subsample=(4,4),activation='relu',
                             init= lambda shape, name: normal(shape, scale= 0.01, name= name),
                             border_mode='same',input_shape=input_shape))
-    model.add(Convolution2D(64,2,2,subsample=(1,1),activation='relu',
+    model.add(Convolution2D(64,2,2,subsample=(2,2),activation='relu',
                             init= lambda shape, name: normal(shape, scale= 0.01, name= name),
                             border_mode='same'))
-    model.add(Convolution2D(64,2,2,subsample=(1,1),activation='relu',
+    model.add(Convolution2D(64,2,2,subsample=(2,2),activation='relu',
                             init= lambda shape, name: normal(shape, scale= 0.01, name= name),
                             border_mode='same'))
     model.add(Flatten())
@@ -50,19 +50,15 @@ def append_to_hist(state, obs):
     for i in range(state.shape[0]-1):
         state[i, :] = state[i+1, :]
     state[-1, :] = obs
-
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# NOTE:
-# In contrast to your last exercise you DO NOT generate data before training
-# instead the TransitionTable is build up while you are training to make sure
-# that you get some data that corresponds roughly to the current policy
-# of your agent
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
 
 # 0. initialization
-
 sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
 model = model()
+
+#continue training from a previous model
+#model.load_weights("model.h5")
+
 # setup a large transitiontable that is filled during training
 maxlen = 100000
 trans = TransitionTable(opt.state_siz, opt.act_num, opt.hist_len,
@@ -83,7 +79,6 @@ state_with_history = np.zeros((opt.hist_len, opt.state_siz))
 append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
 next_state_with_history = np.copy(state_with_history)
 
-# How to decreas the discount fator to make the algorithm converge ?
 
 for step in range(steps):
     if (step < observe):
@@ -98,24 +93,21 @@ for step in range(steps):
         state_with_history[:] = 0
         append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
         next_state_with_history = np.copy(state_with_history)
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # TODO: here you would let your agent take its action
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # this just gets a random action
+        
     epi_step+=1
     loss = 0
 
     ### TAKE AN ACTION ###
-    if random.random() <= epsilon:  #random number between [0,1)
+    if random.random() <= epsilon:
         print("Random")
         action = randrange(opt.act_num)
     else:
         print("Greedy")
-        action = np.argmax(model.predict(np.transpose(state_with_history).reshape(1,img_rows,img_cols,opt.hist_len)))
+        action = np.argmax(model.predict((state_with_history).reshape(1,img_rows,img_cols,opt.hist_len)))
     action_onehot = trans.one_hot_action(action)
 
     #annealing the epsilon
-    if(epsilon>FINAL_EPSILON and step>observe):
+    if(epsilon>FINAL_EPSILON and step>=observe):
         epsilon -= ((INITIAL_EPSILON-FINAL_EPSILON)/explore)
 
     #Take next step according to the action selected
@@ -128,9 +120,6 @@ for step in range(steps):
     # mark next state as current state
     state_with_history = np.copy(next_state_with_history)
     state = next_state
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # TODO: here you would train your agent
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     #start training when at least a number of 'observe' frames are seen
     if(step >= observe):
@@ -142,10 +131,6 @@ for step in range(steps):
         #prediction for Q values of next state from the CNN
         Qn =model.predict_on_batch(next_state_batch.reshape(opt.minibatch_size,img_rows,img_cols,opt.hist_len))
 
-        #Double Q learning (Freazing the updates)
-        # if((step % 1000) == 0):
-        #     Qn =model.predict_on_batch(np.transpose(next_state_batch).reshape(opt.minibatch_size,img_rows,img_cols,opt.hist_len))
-
         #Target Q
         for i in range(0,opt.minibatch_size):
             Q[i,np.argmax(action_batch[i,:])] = ((1-terminal_batch[i,:]) * disc_fact* np.max(Qn[i,:])) + reward_batch[i,:]
@@ -156,6 +141,8 @@ for step in range(steps):
         #Update the weights and calculate the loss accordingly
         loss = model.train_on_batch(state_batch,Q)
 
+        #There is a hyper parameter in the utilss calledd eval_nepisodes check it
+        # do some trajectories with only greedy actions and see the sum of the rewards is decreasing or not
         # TODO every once in a while you should test your agent here so that you can track its performance
         if((step % test_steps) == 0):
             print("Testing")
@@ -163,9 +150,6 @@ for step in range(steps):
             for i in range(0,opt.eval_nepisodes):
                 pass
             #     action = np.argmax(model.predict(np.transpose(state_with_history).reshape(1,img_rows,img_cols,opt.hist_len)))
-
-
-
 
 
         if opt.disp_on:
@@ -183,7 +167,9 @@ for step in range(steps):
             print('Episode %d, step %d, epsilon %.6f, action %d, reward %.5f' %
                   (nepisodes,epi_step,epsilon,action,state.reward))
 
-    #There is a hyper parameter in the utilss calledd eval_nepisodes check it
 
     # 2. perform a final test of your model and save it
     # TODO
+    model.save_weights("model.h5", overwrite=True)
+    with open("model.json", "w") as outfile:
+        json.dump(model.to_json(), outfile)
